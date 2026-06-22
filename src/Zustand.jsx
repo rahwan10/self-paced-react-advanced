a531ae17905dae8f1634b75df0025e4094fadbe4
rahwan10 <donggeon10@naver.com>
2026-06-22 17:45:09 +0900

    feat: persist사용하여 새로고침해도 카테고리 유지되게 구현

--- src/Zustand.jsx
+++ src/Zustand.jsx
@@ -1,7 +1,15 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useCategory = create((set) => ({
-  category: "전체",
-  setCategory: (newCategory) => set({ category: newCategory }),
-}));
+export const useCategory = create(
+  persist(
+    (set) => ({
+      category: "전체",
+      setCategory: (newCategory) => set({ category: newCategory }),
+    }),
 
+    {
+      name: "category-storage",
+    },
+  ),
+);
