37870f7d73b02e073e2d4b2116386a0d568e23ea
rahwan10 <donggeon10@naver.com>
2026-06-22 17:16:46 +0900

    fix:안쓰는 코드 수정

--- src/components/Main/RestaurantList.jsx
+++ src/components/Main/RestaurantList.jsx
@@ -1,6 +1,5 @@
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { useContext } from "react";
 import { useCategory } from "../../UserContext";
 
 import korean from "../../../templates/category-korean.png";
