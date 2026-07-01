e34927ccde634215244574a6975aec51a11fbb42
rahwan10 <donggeon10@naver.com>
2026-07-01 16:47:50 +0900

    feat: query설치 및 프로바이더 씌우기

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
+<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+=======
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+>>>>>>> 54ec02b (feat: query설치 및 프로바이더 씌우기)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
