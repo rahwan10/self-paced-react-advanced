921d8e0a832b59db360c7551804da9276c826368
rahwan10 <donggeon10@naver.com>
2026-07-03 12:46:01 +0900

    fix: 충돌해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,119 +1,82 @@
-<<<<<<< HEAD
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Header from './components/Header/Header';
 import CategoryFilter from './components/Main/CategoryFilter';
 import RestaurantList from './components/Main/RestaurantList';
 import RestaurantDetailModal from './components/Aside/RestaurantDetailModal';
 import AddRestaurantModal from './components/Aside/AddRestaurantModal';
-=======
-import { useState } from "react";
-import Header from "./components/Header/Header";
-import CategoryFilter from "./components/Main/CategoryFilter";
-import RestaurantList from "./components/Main/RestaurantList";
-import RestaurantDetailModal from "./components/Aside/RestaurantDetailModal";
-import AddRestaurantModal from "./components/Aside/AddRestaurantModal";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { useQuery } from "@tanstack/react-query";
->>>>>>> 1897623 (feat: TanStack으로 restaurant 상태를 서버 상태로 관리)
-=======
-import { useQuery, useMutation } from "@tanstack/react-query";
->>>>>>> 0bf3031 (feat: useMutation을 사용해 음식점 추가 기능 구현)
-=======
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
->>>>>>> 9b7088b (feat: query invalidation 적용)
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 
 function App() {
-  // 상태값
   const [category, setCategory] = useState('전체');
-
   const [isDetailModal, setIsDetailModal] = useState(false);
-
   const [selectedRestaurantId, setSelectedRestaurantId] = useState(null);
-
   const [isAddModal, setIsAddModal] = useState(false);
 
   const client = useQueryClient();
 
   async function fetchRestaurants() {
-    const res = await fetch("http://localhost:3000/restaurants");
-    const data = await res.json();
-    return data;
+    const res = await fetch('http://localhost:3000/restaurants');
+    return await res.json();
   }
 
-<<<<<<< HEAD
-  useEffect(() => {
-    fetch('http://localhost:3000/restaurants')
-      .then((res) => res.json())
-      .then((data) => setTotalRestaurants(data));
-  }, []);
-  // 파생값
+  const { data: totalRestaurants = [] } = useQuery({
+    queryKey: ['restaurants'],
+    queryFn: fetchRestaurants,
+  });
+
+  const selectedRestaurant = totalRestaurants.find((r) => r.id === selectedRestaurantId);
+
   const filteredRestaurants =
     category === '전체'
       ? totalRestaurants
       : totalRestaurants.filter((r) => r.category === category);
-=======
-  const { data: totalRestaurants = [] } = useQuery({
-    queryKey: ["restaurants"],
-    queryFn: fetchRestaurants,
+
+  const addRestaurant = async (newRestaurant) => {
+    await fetch('http://localhost:3000/restaurants', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newRestaurant),
+    });
+  };
+
+  const { mutate } = useMutation({
+    mutationFn: addRestaurant,
+
+    onMutate: async (newRestaurant) => {
+      await client.cancelQueries({ queryKey: ['restaurants'] });
+
+      const previousRestaurants = client.getQueryData(['restaurants']);
+
+      client.setQueryData(['restaurants'], (old = []) => [...old, newRestaurant]);
+
+      return { previousRestaurants };
+    },
+
+    onError: (err, newRestaurant, context) => {
+      client.setQueryData(['restaurants'], context.previousRestaurants);
+    },
+
+    onSettled: () => {
+      client.invalidateQueries({ queryKey: ['restaurants'] });
+    },
   });
->>>>>>> 1897623 (feat: TanStack으로 restaurant 상태를 서버 상태로 관리)
 
-  const selectedRestaurant = totalRestaurants.find((r) => r.id === selectedRestaurantId);
-
-  //  핸들러
   const handleClickRestaurantList = (r) => {
     setIsDetailModal(true);
     setSelectedRestaurantId(r.id);
   };
 
-<<<<<<< HEAD
-<<<<<<< HEAD
   const handleClickAddRestaurant = (newRestaurant) => {
-    setIsAddModal(false);
-    setTotalRestaurants((prev) => [...prev, newRestaurant]);
-=======
-  const handleClickAddRestaurant = async (newRestaurant) => {
-=======
-  const addRestaurant = async (newRestaurant) => {
->>>>>>> 0bf3031 (feat: useMutation을 사용해 음식점 추가 기능 구현)
-    await fetch("http://localhost:3000/restaurants", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newRestaurant),
-    });
-  };
-  const { mutate } = useMutation({
-    mutationFn: addRestaurant,
-
-    onMutate: async (newRestaurant) => {
-      await client.cancelQueries({ queryKey: ["restaurants"] });
-      const previousRestaurants = client.getQueryData(["restaurants"]);
-      client.setQueryData(["restaurants"], (old) => [...old, newRestaurant]);
-      return { previousRestaurants };
-    },
-
-    onError: (context) => {
-      client.setQueryData(["restaurants"], context.previousRestaurants);
-    },
-
-    onSettled: () => {
-      client.invalidateQueries({ queryKey: ["restaurants"] });
-    },
-  });
-
-  const handleClickAddRestaurant = async (newRestaurant) => {
     mutate(newRestaurant);
-    console.log("음식점 추가 완료");
     setIsAddModal(false);
->>>>>>> 1897623 (feat: TanStack으로 restaurant 상태를 서버 상태로 관리)
   };
 
   return (
     <>
       <Header setIsAddModal={setIsAddModal} />
+
       <main>
         <CategoryFilter category={category} setCategory={setCategory} />
         <RestaurantList
@@ -121,6 +84,7 @@ function App() {
           handleClickRestaurantList={handleClickRestaurantList}
         />
       </main>
+
       <aside>
         {isDetailModal && (
           <RestaurantDetailModal
@@ -128,6 +92,7 @@ function App() {
             selectedRestaurant={selectedRestaurant}
           />
         )}
+
         {isAddModal && <AddRestaurantModal handleClickAddRestaurant={handleClickAddRestaurant} />}
       </aside>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
->>>>>>> 54ec02b (feat: query설치 및 프로바이더 씌우기)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
